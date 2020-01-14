2ba12ad90f620d9aad8a9a06185832c06209ac9f
rahul <rahul171627.cse@chitkara.edu.in>
2020-01-14 22:03:25 +0530

    creating database and adding tables

---
+++ Entities/ProductLinkText.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+
+namespace Memberships.Entities
+{
+    [Table("ProductLinkText")]
+    public class ProductLinkText
+    {
+        [DatabaseGenerated(DatabaseGeneratedOption.Identity)]
+        public int Id { get; set; }
+
+        [MaxLength(40)]
+        [Required]
+        public string Title { get; set; }
+    }
+}
---
+++ Entities/UserSubscription.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.ComponentModel.DataAnnotations;
+using System.Web;
+
+namespace Memberships.Entities
+{
+    public class UserSubscription
+    {
+        [Required]
+        [Key, Column(Order =1)]
+        public int SubscriptionId { get; set; }
+
+        [Required]
+        [Key, Column(Order = 2)]
+        [MaxLength(128)]
+        public string UserId { get; set; }
+
+        public DateTime? StartDate { get; set; }
+
+        public DateTime? EndDate { get; set; }
+
+
+    }
+}
