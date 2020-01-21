953dcff4164e935f71961ed77b265ba1f2c81af2
rahul <rahul171627.cse@chitkara.edu.in>
2020-01-21 21:39:07 +0530

    adding product controller

---
+++ Areas/Admin/Extension/ConversionExtensions.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using Memberships.Areas.Admin.Models;
+using System.Linq;
+using System.Threading.Tasks;
+using Memberships.Models;
+using Memberships.Entities;
+using System.Data.Entity;
+
+namespace Memberships.Areas.Admin.Extension
+{
+    public static class ConversionExtensions
+    {
+        public static async Task<IEnumerable<ProductModel>> Convert(
+            this IEnumerable<Product> products, ApplicationDbContext db)
+        {
+            if (products.Count().Equals(0))
+                return new List<ProductModel>();
+            var texts = await db.ProductLinkTexts.ToListAsync();
+            var types = await db.ProductTypes.ToListAsync();
+
+            return from p in products
+                   select new ProductModel
+                   {
+                       Id = p.Id,
+                       Title = p.Title,
+                       Description = p.Description,
+                       ImageUrl = p.ImageUrl,
+                       ProductLinkTextId = p.ProductLinkTextId,
+                       ProductTypeId = p.ProductTypeId,
+                       ProductLinkTexts = texts,
+                       ProductTypes = types
+                     };
+        }
+
+    }
+}
